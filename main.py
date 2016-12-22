b4b2fab93aa4e86273eb03ee685c6be1150e3ca8
mariezelenina <marie.zelenina@gmail.com>
2016-12-22 07:41:47 +0000

    playing with initial code

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ trained_model = model.fit(x_train,y_train)
 
 # do predictions
 y_test_predicted = trained_model.predict(x_test)
+print y_test_predicted
 
 # Accuracy score
 print trained_model.score(x_test,y_test)
+# todo Log loss score
 
 
-
+'''
 # make submission file
 x_submit = data_reader('data/5c9fa979-5a84-45d6-93b9-543d1a0efc41.csv', ycolumn=False)
 y_submit_predicted = trained_model.predict(x_submit)
 
-data_writer(x_submit,y_submit_predicted,'data/submissions/LogReg.csv')
+data_writer(x_submit,y_submit_predicted,'data/submissions/LogReg.csv')'''
 
 
